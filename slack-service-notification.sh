d88ce06050334b1132c81cb7d442ebf430ab90a8
Danny Verkade - Cream <danny@cream.nl>
2018-07-31 17:37:27 -0400

    Fixed a typo

--- slack-service-notification.sh
+++ slack-service-notification.sh
@@ -34,7 +34,7 @@ read -d '' PAYLOAD << EOF
       "fields": [
         {
           "title": "Service output",
-          "value": "${SERVICEPUTPUT}",
+          "value": "${SERVICEPOUTPUT}",
           "short": false
         },
         {
