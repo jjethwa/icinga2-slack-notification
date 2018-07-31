ef0879bcb7ed45fcedcb67929185ff376e21491a
Danny Verkade - Cream <danny@cream.nl>
2018-07-31 17:31:31 -0400

    Better template for service notifications
    - Created a better template for service notification using attachments. 
    - Added ability to use normal JSON instead of the need to escape it.

--- slack-service-notification.sh
+++ slack-service-notification.sh
@@ -8,21 +8,56 @@ SLACK_BOTNAME="icinga2"
 #Set the message icon based on ICINGA service state
 if [ "$SERVICESTATE" = "CRITICAL" ]
 then
-    ICON=":bomb:"
+    COLOR="danger"
 elif [ "$SERVICESTATE" = "WARNING" ]
 then
-    ICON=":warning:"
+    COLOR="warning"
 elif [ "$SERVICESTATE" = "OK" ]
 then
-    ICON=":beer:"
+    COLOR="good"
 elif [ "$SERVICESTATE" = "UNKNOWN" ]
 then
-    ICON=":question:"
+    COLOR="#800080"
 else
-    ICON=":white_medium_square:"
+    COLOR=""
 fi
 
 #Send message to Slack
-PAYLOAD="payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_BOTNAME}\", \"text\": \"${ICON} HOST: <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}>   SERVICE: <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>  STATE: ${SERVICESTATE}\"}"
+read -d '' PAYLOAD << EOF
+{
+  "channel": "${SLACK_CHANNEL}",
+  "username": "${SLACK_BOTNAME}",
+  "attachments": [
+    {
+      "fallback": "${SERVICESTATE}: ${HOSTDISPLAYNAME} - ${SERVICEDISPLAYNAME}",
+      "color": "${COLOR}",
+      "fields": [
+        {
+          "title": "Service output",
+          "value": "${SERVICEPUTPUT}",
+          "short": false
+        },
+        {
+          "title": "Host",
+          "value": "<${ICINGA_HOSTNAME}/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}>",
+          "short": true
+        },
+        {
+          "title": "Service",
+          "value": "<${ICINGA_HOSTNAME}/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>",
+          "short": true
+        },
+        {
+          "title": "State",
+          "value": "${SERVICESTATE}",
+          "short": true
+        }
+      ]
+    }
+  ]
+}
+EOF
 
-curl --connect-timeout 30 --max-time 60 -s -S -X POST --data-urlencode "${PAYLOAD}" "${SLACK_WEBHOOK_URL}"
+echo ${PAYLOAD}
+
+curl --connect-timeout 30 --max-time 60 -s -S -X POST -H 'Content-type: application/json' --data "${PAYLOAD}" "${SLACK_WEBHOOK_URL}"
