c8652a623a95c21c432c62856d751255f1ab9b44
OJ LaBoeuf <orangepeelbeef@gmail.com>
2019-08-05 12:12:26 -0700

    Add DOWNTIMESTART/END and ACKNOWLEDGE

--- slack-host-notification.sh
+++ slack-host-notification.sh
@@ -5,6 +5,47 @@ SLACK_WEBHOOK_URL="<YOUR_SLACK_WEBHOOK_INTEGRATION_URL>"
 SLACK_CHANNEL="#alerts"
 SLACK_BOTNAME="icinga2"
 
+
+if [ "$NOTIFICATIONTYPE" = "ACKNOWLEDGEMENT" ] || [ "$NOTIFICATIONTYPE" = "DOWNTIMESTART" ] || [ "$NOTIFICATIONTYPE" = "DOWNTIMEEND" ]
+then
+  COLOR="#33FFE3"
+#Send message to Slack
+read -d '' PAYLOAD << EOF
+{
+  "channel": "${SLACK_CHANNEL}",
+  "username": "${SLACK_BOTNAME}",
+  "attachments": [
+    {
+      "fallback": "${NOTIFICATIONTYPE} ${HOSTSTATE}: ${HOSTDISPLAYNAME}",
+      "color": "${COLOR}",
+      "fields": [
+        {
+          "title": "${NOTIFICATIONTYPE}",
+          "value": "${NOTIFICATIONCOMMENT} - ${NOTIFICATIONAUTHORNAME}"
+        },
+        {
+          "title": "Info",
+          "value": "${HOSTOUTPUT}",
+          "short": false
+        },
+        {
+          "title": "Host",
+          "value": "<${ICINGA_HOSTNAME}/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}>",
+          "short": true
+        },
+        {
+          "title": "State",
+          "value": "${HOSTSTATE}",
+          "short": true
+        }
+      ]
+    }
+  ]
+}
+EOF
+else
+
+
 #Set the message icon based on ICINGA Host state
 if [ "$HOSTSTATE" = "DOWN" ]
 then
--- slack-service-notification.sh
+++ slack-service-notification.sh
@@ -5,6 +5,50 @@ SLACK_WEBHOOK_URL="<YOUR_SLACK_WEBHOOK_INTEGRATION_URL>"
 SLACK_CHANNEL="#alerts"
 SLACK_BOTNAME="icinga2"
 
+
+if [ "$NOTIFICATIONTYPE" = "ACKNOWLEDGEMENT" ] || [ "$NOTIFICATIONTYPE" = "DOWNTIMESTART" ] || [ "$NOTIFICATIONTYPE" = "DOWNTIMEEND" ]
+then
+  COLOR="#FFB6C1"
+  read -d '' PAYLOAD << EOF
+{
+  "channel": "${SLACK_CHANNEL}",
+  "username": "${SLACK_BOTNAME}",
+  "attachments": [
+    {
+      "fallback": "${NOTIFICATIONTYPE}: ${SERVICESTATE}: ${HOSTDISPLAYNAME} - ${SERVICEDISPLAYNAME}",
+      "color": "${COLOR}",
+      "fields": [
+        {
+          "title": "${NOTIFICATIONTYPE}",
+          "value": "${NOTIFICATIONCOMMENT} - ${NOTIFICATIONAUTHORNAME}"
+        },
+        {
+          "title": "Service output",
+          "value": "${SERVICEOUTPUT}",
+          "short": false
+        },
+        {
+          "title": "Host",
+          "value": "<${ICINGA_HOSTNAME}/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}>",
+          "short": true
+        },
+        {
+          "title": "Service",
+          "value": "<${ICINGA_HOSTNAME}/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>",
+          "short": true
+        },
+        {
+          "title": "State",
+          "value": "${SERVICESTATE}",
+          "short": true
+        }
+      ]
+    }
+  ]
+}
+EOF
+else
+
 #Set the message icon based on ICINGA service state
 if [ "$SERVICESTATE" = "CRITICAL" ]
 then
@@ -57,5 +101,7 @@ read -d '' PAYLOAD << EOF
   ]
 }
 EOF
+
+fi
 
 curl --connect-timeout 30 --max-time 60 -s -S -X POST -H 'Content-type: application/json' --data "${PAYLOAD}" "${SLACK_WEBHOOK_URL}"
